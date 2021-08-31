685b580830db2de3f67f4655d883827faa0099b4
Sean <prestosean@myvuw.ac.nz>
2021-09-01 04:19:10 +1200

    Added starting class structure for Renderer module:
    - MazeView: Custom JPanel for drawing graphics to window
    - ElementAnimator: Base abstract class for any type of element to be animated
         - TileAnimator: Responsible for animating tiles such as walls, doors & floor space tiles.
         - ItemAnimator: Responsible for animating objects that can be picked up by player (may be merged with TileAnimator should they remain functionally identical)
         - ActorAnimator: Responsible for animating actors on screen, that need to move between tiles.

---
+++ src/nz/ac/vuw/ecs/swen225/gp21/renderer/MazeView.java
@@ -0,0 +1,16 @@
+package nz.ac.vuw.ecs.swen225.gp21.renderer;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MazeView extends JPanel {
+
+    @Override
+    protected void paintComponent(Graphics g) {
+
+        //Clear Background
+        super.paintComponent(g);
+
+    }
+
+}
