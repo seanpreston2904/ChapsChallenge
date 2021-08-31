6ece0b897a04c5577b7a038826302f0329698582
NRais <raisnath@ecs.vuw.ac.nz>
2021-08-31 23:45:51 +1200

    Added new Actor interface and children

---
+++ src/nz/ac/vuw/ecs/swen225/gp21/domain/actor/Actor.java
@@ -0,0 +1,8 @@
+package nz.ac.vuw.ecs.swen225.gp21.domain.actor;
+
+/**
+ * Actor is the interface that movable actors in the levels will inherit
+ */
+public interface Actor {
+
+}
---
+++ src/nz/ac/vuw/ecs/swen225/gp21/domain/actor/Player.java
@@ -0,0 +1,4 @@
+package nz.ac.vuw.ecs.swen225.gp21.domain.actor;
+
+public class Player implements Actor {
+}
