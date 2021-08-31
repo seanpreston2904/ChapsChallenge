0cba87b0a56807100c7fb36569158f11e2130582
NRais <raisnath@ecs.vuw.ac.nz>
2021-08-31 20:55:41 +1200

    Create Tile.java
    Tile is an abstract class representing the types of tiles that you find on the board.

---
+++ src/nz/ac/vuw/ecs/swen225/gp21/domain/board/Tile.java
@@ -0,0 +1,30 @@
+package nz.ac.vuw.ecs.swen225.gp21.domain.board;
+
+import java.awt.*;
+
+public abstract class Tile {
+
+    public enum TileType {
+        WALL, FREE, DOOR, EXIT
+    }
+
+    Dimension location;
+    TileType type;
+
+    /**
+     * Constructor
+     */
+    public Tile(Dimension location, TileType type) {
+        this.location = location;
+        this.type = type;
+    }
+
+    /**
+     * @return - location and type
+     */
+    @Override
+    public String toString() {
+        return "Tile{" + location +
+                ", " + type + '}';
+    }
+}
