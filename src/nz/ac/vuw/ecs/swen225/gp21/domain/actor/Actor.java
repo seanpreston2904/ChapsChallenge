fb5cd379682611e4ee055539006b0be37f2ad0d7
NRais <raisnath@ecs.vuw.ac.nz>
2021-09-02 21:21:57 +1200

    Updated abstract actor and children
    Made Actor an abstract class and added utility methods for use across children

--- src/nz/ac/vuw/ecs/swen225/gp21/domain/actor/Actor.java
+++ src/nz/ac/vuw/ecs/swen225/gp21/domain/actor/Actor.java
@@ -1,8 +1,49 @@
 package nz.ac.vuw.ecs.swen225.gp21.domain.actor;
 
+import nz.ac.vuw.ecs.swen225.gp21.domain.utils.Coordinate;
+import nz.ac.vuw.ecs.swen225.gp21.domain.utils.Direction;
+
 /**
  * Actor is the interface that movable actors in the levels will inherit
  */
-public interface Actor {
+public abstract class Actor {
+
+    private Coordinate position;
+
+    public Actor(Coordinate initial) {
+        this.position = initial;
+    }
+
+    /**
+     * Utility method to move the actor a certain direction and return its resulting position
+     *
+     * @param direction the direction to move to
+     * @return
+     */
+    public Coordinate getResultingLocation(Direction direction) {
+
+        // in favor of the traditional and confusing Swing system (south is +1)
+        switch (direction) {
+            case EAST:
+                return new Coordinate(position.getX() + 1, position.getY());
+            case SOUTH:
+                return new Coordinate(position.getX(), position.getY() + 1);
+            case WEST:
+                return new Coordinate(position.getX() - 1, position.getY());
+            case NORTH:
+                return new Coordinate(position.getX(), position.getY() - 1);
+        }
+
+        return null;
+    }
+
+    /**
+     * setter for the object's location
+     *
+     * @param position
+     */
+    public void setPosition(Coordinate position) {
+        this.position = position;
+    }
 
 }
--- src/nz/ac/vuw/ecs/swen225/gp21/domain/actor/Player.java
+++ src/nz/ac/vuw/ecs/swen225/gp21/domain/actor/Player.java
@@ -1,4 +1,16 @@
 package nz.ac.vuw.ecs.swen225.gp21.domain.actor;
 
-public class Player implements Actor {
+import nz.ac.vuw.ecs.swen225.gp21.domain.board.Item;
+import nz.ac.vuw.ecs.swen225.gp21.domain.utils.Coordinate;
+
+import java.util.ArrayList;
+
+public class Player extends Actor {
+
+    private ArrayList<Item> inventory;
+
+    public Player(Coordinate initial) {
+        super(initial);
+    }
+
 }
