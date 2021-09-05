cfdc56df0bdb4f3d24ea14a7f9ebf354236c4b8c
rae <jkraezou@gmail.com>
2021-09-06 11:06:11 +1200

    initialized the methods in WriteToXML.java.

---
+++ src/nz/ac/vuw/ecs/swen225/gp21/persistency/WriteToXML.java
@@ -0,0 +1,32 @@
+package nz.ac.vuw.ecs.swen225.gp21.persistency;
+
+import org.dom4j.Element;
+
+/**
+ * writing the Java objects to XML files to save the current game state in order for the player to resume games.
+ *
+ * @author Rae 300535154
+ */
+public class WriteToXML {
+
+    /**
+     * convert the game objects to XML file.
+     * all objects are from the board.
+     */
+    public void writeGameToXML(){
+
+    }
+
+    public void objectsToXML(Element root){
+
+    }
+
+    public void addNodes(Element root, String type, String x, String y, String item, String col) {
+
+    }
+
+
+    public void addWalls(Element root){
+
+    }
+}
