5b3dff2a80e9caa8771e7ff160e2514d020d01f0
NRais <raisnath@ecs.vuw.ac.nz>
2021-09-05 13:43:40 +1200

    Added Items
    - Added 5 types of Items
    - Concreted their interaction interface and methods
    - Added a player inventory

--- src/nz/ac/vuw/ecs/swen225/gp21/domain/board/Item.java
+++ src/nz/ac/vuw/ecs/swen225/gp21/domain/board/Item.java
@@ -1,9 +1,11 @@
 package nz.ac.vuw.ecs.swen225.gp21.domain.board;
 
+import nz.ac.vuw.ecs.swen225.gp21.domain.actor.Actor;
+
 public abstract class Item {
 
     public enum ItemType {
-        KEY, TREASURE, INFO, EXIT_LOCK, DOOR_LOCK
+        KEY, TREASURE, INFO, LOCK_DOOR, LOCK_EXIT
     }
 
     private ItemType type;
@@ -20,7 +22,14 @@ public abstract class Item {
     /**
      * The key method of the item class is what happens when you interact. This is declared in the objects
      */
-    public abstract void interact();
+    public abstract void interact(Actor actor);
+
+    /**
+     * In order to locate items in the inventory we need n ID
+     */
+    public String getId() {
+        return "ID: " + type;
+    }
 
     /**
      * get type
@@ -36,6 +45,7 @@ public abstract class Item {
     public boolean isImpassable() {
         return impassable;
     }
+
 
     /**
      * @return type
---
+++ src/nz/ac/vuw/ecs/swen225/gp21/domain/board/Item_Key.java
@@ -0,0 +1,41 @@
+package nz.ac.vuw.ecs.swen225.gp21.domain.board;
+
+import nz.ac.vuw.ecs.swen225.gp21.domain.actor.Actor;
+import nz.ac.vuw.ecs.swen225.gp21.domain.actor.Player;
+
+import java.awt.*;
+
+public class Item_Key extends Item {
+
+    private Color color;
+
+    /**
+     * Constructor initializes type KEY and impassable = false
+     */
+    public Item_Key(Color color) {
+        super(ItemType.KEY, false);
+        this.color = color;
+    }
+
+    /**
+     * Interact void fires upon player entering the cell
+     *
+     * Key is added to inventory
+     */
+    @Override
+    public void interact(Actor actor) {
+
+        // try to cast Actor to Player
+        if (actor instanceof Player) {
+
+            Player player = (Player)actor;
+
+            // add to inventory
+            player.addToInventory(this);
+
+            // remove from board
+            // TODO
+        }
+
+    }
+}
