5fefc863c29b296f03632740105f07e30c6d57d5
Nguyen Van <vannguy1@ecs.vuw.ac.nz>
2021-09-10 15:27:42 +1200

    Update on countdown panel to draw a countdown panel on the main frame

--- src/nz/ac/vuw/ecs/swen225/gp21/app/CountdownPanel.java
+++ src/nz/ac/vuw/ecs/swen225/gp21/app/CountdownPanel.java
@@ -1,5 +1,6 @@
-package nz.ac.vuw.ecs.swen225.gp21.recorder;
+package nz.ac.vuw.ecs.swen225.gp21.app;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
@@ -10,29 +11,30 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JLayeredPane;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.Timer;
 
-import board.Board;
 
 public class CountdownPanel implements ActionListener {
 	
-	private Board board;
-	private int seconds_remaining = 100;
+//	private Board board;
+	private int seconds_remaining = 5;
 	private boolean started = false;
-	
-	private JFrame frame = new JFrame();	
-	private JLayeredPane panel = new JLayeredPane(); 
+		
+	private JLayeredPane countdownPanel = new JLayeredPane(); 
 	private JLabel time_left = new JLabel();
 	private JLabel level = new JLabel();
 	private JLabel chips_left = new JLabel();
-    private JButton start_pause = new JButton("Start");
+    private JButton start_pause = new JButton("START");
+   
 	
 	Timer timer = new Timer(1000, new ActionListener() {
 		
 		@Override
 		public void actionPerformed(ActionEvent e) {
-
+			
 			seconds_remaining = seconds_remaining-1;
 			String seconds_string = String.format(String.valueOf(seconds_remaining));
 			time_left.setText(seconds_string);	  
@@ -40,6 +42,10 @@ public class CountdownPanel implements ActionListener {
 			// if timer reaches 0, the player looses
 			if(seconds_remaining==0) {
 				timer.stop();
+				int confirm = JOptionPane.showOptionDialog(
+                        null, "You have ran out of time",
+                        "Exit Confirmation", JOptionPane.YES_NO_OPTION,
+                        JOptionPane.QUESTION_MESSAGE, null, null, null);
 			}
 			
 		}});
@@ -53,53 +59,76 @@ public class CountdownPanel implements ActionListener {
      * @param remaining_chips number of chips on board in the level
      */
 	
-	public CountdownPanel(int time, int current_level, int chipsleft, Board board) {
-		
-		this.board = board;
-		start_pause.addActionListener(this);
-		start_pause.setFocusable(false);
-		seconds_remaining = time;
+	public CountdownPanel(int time, int current_level, int chipsleft) {
 		
 		//this.board = board;
-		panel.setSize(300, 600);		
-		        
+		start_pause.addActionListener(this);
+		start_pause.setFocusable(false);
+		seconds_remaining = time;	
+		
         // the title of each number boxes (TIME, LEVEL, CHIPSLEFT)
-		JLabel timer_title = buttonTitleSetup("TIME");		
-        JLabel chips_left_title = buttonTitleSetup("CHIPS LEFT");        		        
-        JLabel level_title = buttonTitleSetup("LEVEL");
+		JLabel timer_title = titleLabel("TIME", Color.RED);		
+        JLabel chips_title = titleLabel("CHIPS",Color.RED);  
+        JLabel left_title  = titleLabel("LEFT", Color.RED);
+        JLabel level_title = titleLabel("LEVEL",Color.RED);
+        
+		JLabel timer_title2 = titleLabel("TIME", Color.WHITE);		
+        JLabel chips_title2 = titleLabel("CHIPS",Color.WHITE);  
+        JLabel left_title2  = titleLabel("LEFT", Color.WHITE);
+        JLabel level_title2 = titleLabel("LEVEL",Color.WHITE);
+        
+        
         
         // the JLabels that contains all the value
         time_left = buttonTitleSetup(String.valueOf(time));	        
         chips_left = buttonTitleSetup(String.valueOf(chipsleft));        		        
         level = buttonTitleSetup(String.valueOf(current_level));     
         
-       
         //setting the location of the Jlabels
         //----------------------------------------
-        timer_title.setBounds(200, 50, 96, 40);
-        time_left.setBounds(200, 100, 96, 40);
-        chips_left_title.setBounds(200, 150, 96, 40);
-        chips_left.setBounds(200, 200, 96, 40);
-        level_title.setBounds(200, 250, 96, 40);
-        level.setBounds(200, 300, 96, 40);
-        start_pause.setBounds(200, 400, 90, 40);
+        timer_title.setBounds(75, 30, 96, 35);
+        timer_title2.setBounds(76, 28, 96, 35);
+        time_left.setBounds(55, 65, 96, 40);
+        chips_title.setBounds(75, 115, 96, 35);
+        chips_title2.setBounds(76, 113, 96, 35);
+        left_title.setBounds(80,140,96,35);
+        left_title2.setBounds(81,138,96,35);
+        chips_left.setBounds(55, 175, 96, 40);
+        level_title.setBounds(72, 225, 96, 35);
+        level_title2.setBounds(73, 223, 96, 35);
+        level.setBounds(55, 260, 96, 40);
+        start_pause.setBounds(60, 350, 90, 40);
         //----------------------------------------
         
-        panel.add(timer_title);
-        panel.add(time_left);
-        panel.add(level_title);
-        panel.add(level);
-        panel.add(chips_left_title);
-        panel.add(chips_left);    
-        panel.add(start_pause);
+        countdownPanel.add(timer_title);
+        countdownPanel.add(time_left);
+        countdownPanel.add(level_title);
+        countdownPanel.add(level);
+        countdownPanel.add(chips_title);
+        countdownPanel.add(left_title);
+        countdownPanel.add(chips_left);    
+        countdownPanel.add(start_pause);
+        countdownPanel.add(timer_title2);
+        countdownPanel.add(chips_title2);
+        countdownPanel.add(left_title2);
+        countdownPanel.add(level_title2);
         
-        frame.add(panel);
+        JPanel backgroundPan = new JPanel();
+        backgroundPan.setBackground(Color.LIGHT_GRAY);
+        backgroundPan.setBounds(5,5,195,445);
         
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(600,600);
-        frame.setLayout(null);
-        frame.setVisible(true);			
+        countdownPanel.add(backgroundPan);
+        
+        
+                           
+	}	
+	
+	public JLayeredPane getPanel() {
+		return countdownPanel;
 	}
+	
+
+	
 	
 	/**
      * This method sets up JLabel for all the time, chips left, level labels on the panel 
@@ -111,12 +140,22 @@ public class CountdownPanel implements ActionListener {
 		
 		JLabel label = new JLabel(title);   
 		label.setFont(new Font("Roboto",Font.BOLD,20));
-		label.setForeground(Color.RED);
-		label.setOpaque(false);
+		label.setForeground(Color.BLACK);
+		label.setOpaque(true);
 		label.setBorder(BorderFactory.createBevelBorder(1));
-		label.setHorizontalAlignment(JTextField.CENTER);
+		label.setHorizontalAlignment(JTextField.RIGHT);
 		
 		return label;		
+		
+	}
+	
+	public JLabel titleLabel(String title, Color col) {
+		
+		JLabel label = new JLabel(title);   
+		label.setForeground(col);
+		label.setFont(new Font("Roboto",Font.BOLD,20));		
+		
+		return label;	
 		
 	}
 	
@@ -140,12 +179,10 @@ public class CountdownPanel implements ActionListener {
 		  if(e.getSource() == start_pause) {
 			   
 			   if(started==false) {
-				   start();
-				   start_pause.setText("PAUSE");
+				   start();				   
 			   }
 			   else {
-				   stop();
-				   start_pause.setText("START");
+				   pause();
 			   }
 			   
 			  }
@@ -159,6 +196,7 @@ public class CountdownPanel implements ActionListener {
 	public void start() {
 		started = true;
 		timer.start();
+		start_pause.setText("PAUSE");
 	}
 	
 	/**
@@ -171,7 +209,8 @@ public class CountdownPanel implements ActionListener {
 		
 		// TODO
 		// pop up a message and reset the level and making a new game
-		// board.reset() ??
+		// after that reset the game
+
 		
 	}
 	
@@ -180,7 +219,18 @@ public class CountdownPanel implements ActionListener {
      */
 	
 	public void pause() {
+		start_pause.setText("START");
 		started = false;
 		timer.stop();
 	}	
+	
+	public boolean getStarted() {
+		return started;
+	}
+	
+	public static void main(String[] args) {
+		
+		new App(null);
+		
+	}
 }
