1670f55f9803bfe04dc97b53367d8ea269108510
Nguyen Van <vannguy1@ecs.vuw.ac.nz>
2021-09-10 15:30:32 +1200

    Implemented some actions for each key press

--- src/nz/ac/vuw/ecs/swen225/gp21/app/KeyPresses.java
+++ src/nz/ac/vuw/ecs/swen225/gp21/app/KeyPresses.java
@@ -1,111 +1,116 @@
-package app;
+package nz.ac.vuw.ecs.swen225.gp21.app;
 
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-import domain.Board;
-import domain.Direction;
-import domain.Enemy;
-import domain.Player;
+import nz.ac.vuw.ecs.swen225.gp21.domain.Domain;
+import nz.ac.vuw.ecs.swen225.gp21.domain.actor.Enemy;
+import nz.ac.vuw.ecs.swen225.gp21.domain.actor.Player;
+import nz.ac.vuw.ecs.swen225.gp21.domain.board.Board;
+import nz.ac.vuw.ecs.swen225.gp21.domain.utils.Direction;
+
+
 
 
 public class KeyPresses implements KeyListener {
 	
 	private Board board;
-	private Player player = null;
+	private Player hero = new Player(null);
 	private Enemy enemy = null;
+	private Domain domain;
+	private App app;
+	private CountdownPanel countdownPanel;
+	private JFrame frame;
 	
-	public KeyPresses(Board board, JFrame frame) {
+	public KeyPresses(App app, JFrame frame, Domain domain) {
 		
-		this.board = board;
+		this.domain = domain;
+		//this.hero = domain.hero;
+		//this.board = domain.board;
+	
+		this.frame = frame;
+		this.app = app;
+		this.countdownPanel = app.getCoundownPanel();
 		
-//		this.player = board.getPlayer();
-//		if(board.getEnemy()!=null) {
-//			this.enemy = board.getEnemy();
-//		}
-		
-		frame.addKeyListener(this);
+		frame.addKeyListener(this);		
 		
 	}
 
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		
-//		Coordinate playerCurrentPos = board.getPlayer().
-		
+				
 		int key = e.getKeyCode();
 		
-		//when key pressed is right
-		if(key == 37) {
-			movePlayer(Direction.EAST);
-		}
-		
-		//when key pressed is up
-		if(key == 38) {
-			movePlayer(Direction.NORTH);								
-		}
-				
-		//when key pressed is left
-		if(key == 39) {
-			movePlayer(Direction.WEST);							
-		}
-		
-		//when key pressed is down
-		if(key == 40) {
-			movePlayer(Direction.SOUTH);								
-		}
-		
-		//when key pressed is space
-		if(key == 32) {
-			movePlayer(Direction.EAST);								
-		}
-		
-		//when key pressed is ecs
-		if(key == 27) {
-											
-		}
+		//only perform action when the game is not paused
+		if(countdownPanel.getStarted()) {
+			if(key == 37) {
+				domain.moveActor(Direction.EAST);
+				//TODO redraw the board in 2d
 			
-		//CTRL-X is pressed to exit game no save
-		if ((e.getKeyCode() == KeyEvent.VK_X) && ((e.getModifiersEx() & KeyEvent.CTRL_DOWN_MASK) != 0)) {
+			}
+		
+			if(key == 38) {
+				System.out.println("hello");
+				domain.moveActor(Direction.NORTH);	
+			}
+				
+			if(key == 39) {
+				domain.moveActor(Direction.EAST);
+			}
+		
+			if(key == 40) {
+				domain.moveActor(Direction.SOUTH);
+			}
+			
+			if(key == 32) {
+				countdownPanel.pause();							
+			}
+		
+			//ecs key pressed to close the "game is paused" dialogue
+			if(key == 27) {
+				countdownPanel.start();								
+			}
+			
+			//CTRL-X is pressed to exit game no save
+			if ((e.getKeyCode() == KeyEvent.VK_X) && ((e.getModifiersEx() & KeyEvent.CTRL_DOWN_MASK) != 0)) {
+				frame.dispose();
+			}
+		
+			//CTRL-S is pressed to save and exit the game
+			if ((e.getKeyCode() == KeyEvent.VK_S) && ((e.getModifiersEx() & KeyEvent.CTRL_DOWN_MASK) != 0)) {
+				//TODO save the game
+				frame.dispose();
+			}
+		
+			//CTRL-R resume a saved game
+			if ((e.getKeyCode() == KeyEvent.VK_R) && ((e.getModifiersEx() & KeyEvent.CTRL_DOWN_MASK) != 0)) {
+			
+			}
+		
+			//CTRL-1 start new game lv1
+			if ((e.getKeyCode() == 49) && ((e.getModifiersEx() & KeyEvent.CTRL_DOWN_MASK) != 0)) {
+				frame.dispose();
+//       	 	XMLFileReader fileReader = new XMLFileReader();
+//   	     	this.board = loadOriginGame("src/nz/ac/vuw/ecs/swen225/gp21/persistency/levels/level1.xml");
+				new App(board);            	           	          	
 
-        }
+			}
 		
-		//CTRL-S is pressed to save and exit the game
-		if ((e.getKeyCode() == KeyEvent.VK_S) && ((e.getModifiersEx() & KeyEvent.CTRL_DOWN_MASK) != 0)) {
-
-        }
+			//CTRL-2 start new game lv2
+			if ((e.getKeyCode() == 50) && ((e.getModifiersEx() & KeyEvent.CTRL_DOWN_MASK) != 0)) {
+				System.out.println("ctrl-2");
+			}
+		}
+		else {
+			System.out.println("hahahahahahahah");
+		}
 		
-		//CTRL-R resume a saved game
-		if ((e.getKeyCode() == KeyEvent.VK_R) && ((e.getModifiersEx() & KeyEvent.CTRL_DOWN_MASK) != 0)) {
- 
-        }
-		
-		//CTRL-1 resume a saved game
-		if ((e.getKeyCode() == 49) && ((e.getModifiersEx() & KeyEvent.CTRL_DOWN_MASK) != 0)) {
-		    System.out.println("ctrl-1");
-        }
-		
-		//CTRL-2 resume a saved game
-		if ((e.getKeyCode() == 50) && ((e.getModifiersEx() & KeyEvent.CTRL_DOWN_MASK) != 0)) {
-		    System.out.println("ctrl-2");
-        }
 		
 	}
+	
 
-	public void movePlayer(Direction direction) {
-		// move the player
-		board.movePlayer(direction);		
-		
-		//if player's next position has a movable tile move the tile too
-	
-	}
-	
-	
-	
-	
-	
 	@Override
 	public void keyReleased(KeyEvent e) {
 		
