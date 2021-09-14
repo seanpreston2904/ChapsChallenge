c3847088f59e8801313de2c8e8a47c14fbe44af4
Nguyen Van <vannguy1@ecs.vuw.ac.nz>
2021-09-15 00:25:05 +1200

    Added JavaDocs

---
+++ src/nz/ac/vuw/ecs/swen225/gp21/app/Main.java
@@ -0,0 +1,11 @@
+package nz.ac.vuw.ecs.swen225.gp21.app;
+
+/**
+ * This class is used to start a game.
+ */
+public class Main{		
+	public static void main(String[] args) {
+		new TitleScreen();	
+	}
+	
+}
