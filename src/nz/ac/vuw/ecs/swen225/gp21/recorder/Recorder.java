88305e17586c67f403abc247691c1e44a38fc631
armstrsamu <armstrsamu@ecs.vuw.ac.nz>
2021-09-14 16:27:44 +1200

    Method stubs
    Added method stubs for the first two methods after discussing with van

--- src/nz/ac/vuw/ecs/swen225/gp21/recorder/Recorder.java
+++ src/nz/ac/vuw/ecs/swen225/gp21/recorder/Recorder.java
@@ -1,10 +1,22 @@
 package nz.ac.vuw.ecs.swen225.gp21.recorder;
 
+import java.util.Stack;
+
 /**
  * **/
 public class Recorder {
 	
+	Stack<Integer> actionStack = new Stack<Integer>();
 	public Recorder() {
+		
+	}
+	
+	public void saveAction(int key) {
+		
+		
+	}
+	
+	public void replay() {//Discuss return type later
 		
 	}
 }
