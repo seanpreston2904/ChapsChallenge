b5ab1aa1b34e7affbb2e2c03f6ea9b14e7945076
Nguyen Van <vannguy1@ecs.vuw.ac.nz>
2021-09-17 00:40:48 +1200

    Added some JOptionPane to some key presses

--- src/nz/ac/vuw/ecs/swen225/gp21/app/KeyPresses.java
+++ src/nz/ac/vuw/ecs/swen225/gp21/app/KeyPresses.java
@@ -16,27 +16,21 @@ import nz.ac.vuw.ecs.swen225.gp21.persistency.XMLFileWriter;
  * Class for the key presses implementation.
  */
 public class KeyPresses implements KeyListener {
-	
-//	private Board board;
+
 	private Player hero;
 	private Domain domain;
 	private CountdownPanel countdownPanel;
-	private JFrame frame;
+	private App app;
 	
-	public KeyPresses(App app, JFrame frame, Domain domain) {
-		if(frame == null) {
-			System.out.println("frame is null");
-		}
+	public KeyPresses(App app, Domain domain) {
+		
+		this.app = app;
 		this.domain = domain;
-		this.hero = domain.getPlayer();
-//		this.board = domain.board;
-	
-		this.frame = frame;
-//		this.app = app;
+		this.hero = domain.getPlayer();	
 		this.countdownPanel = app.getCoundownPanel();
 		
-		frame.addKeyListener(this);
-		
+		app.getMainFrame().addKeyListener(this);
+	
 	}
 
 	/**
@@ -48,69 +42,90 @@ public class KeyPresses implements KeyListener {
 	public void keyPressed(KeyEvent e) {
 				
 		int key = e.getKeyCode();
-		
+
+		/*----------------CTRL-1 start new game lv1-----------------------*/
 		if ((e.getKeyCode() == 49) && ((e.getModifiersEx() & KeyEvent.CTRL_DOWN_MASK) != 0)) {
 			
-			frame.dispose();
-   	 		XMLFileReader fileReader = new XMLFileReader();
-   	 		Board new_board = fileReader.loadOriginGame("src/nz/ac/vuw/ecs/swen225/gp21/persistency/levels/level1.xml");
-			new App(new_board);  
+			countdownPanel.pause();
 			
+            int confirm = JOptionPane.showOptionDialog(
+                    null, "Restart level 1?",
+                    "", JOptionPane.YES_NO_OPTION,
+                    JOptionPane.QUESTION_MESSAGE, null, null, null);
+            
+            // yes = 0, no = 1
+            if(confirm == 0) {           	
+            	app.terminateFrame();
+            	new App("level1");           	
+            }
+            			
 		}
 	
-		//CTRL-2 start new game lv2
+		/*----------------CTRL-2 start new game lv2-----------------------*/
 		if ((e.getKeyCode() == 50) && ((e.getModifiersEx() & KeyEvent.CTRL_DOWN_MASK) != 0)) {
+			countdownPanel.pause();
 			
-			frame.dispose();
-   	 		XMLFileReader fileReader = new XMLFileReader();
-   	 		Board new_board = fileReader.loadOriginGame("src/nz/ac/vuw/ecs/swen225/gp21/persistency/levels/level2.xml");
-			new App(new_board);  
+            int confirm = JOptionPane.showOptionDialog(
+                    app.getMainFrame(), "Restart level 2?",
+                    "", JOptionPane.YES_NO_OPTION,
+                    JOptionPane.QUESTION_MESSAGE, null, null, null);
+            
+            // yes = 0, no = 1
+            if(confirm == 0) {           	
+            	app.terminateFrame();
+            	new App("level2");           	
+            }
 			
 		}
 		
 		//only perform action when the game is not paused
 		if(countdownPanel.getStarted()) {
 			if(key == 37) {
-				domain.moveActor(this.hero, Direction.EAST);
-				//TODO redraw the board in 2d
-				
+				domain.moveActor(this.hero, Direction.WEST);
+				// TODO get the remaining chips to update the countdownPanel.
+				app.updateChipsCount();
 			}
 		
 			if(key == 38) {
-				System.out.println("hello");
-				domain.moveActor(this.hero, Direction.NORTH);	
+				domain.moveActor(this.hero, Direction.NORTH);
+				app.updateChipsCount();
 			}
 				
 			if(key == 39) {
 				domain.moveActor(this.hero, Direction.EAST);
+				app.updateChipsCount();
 			}
-		
+
 			if(key == 40) {
 				domain.moveActor(this.hero, Direction.SOUTH);
+				app.updateChipsCount();
 			}
 			
+			//SPACE
 			if(key == 32) {
-				countdownPanel.pause();							
+				
+				if(countdownPanel.getStarted()) {
+					// TODO pop up a message
+					countdownPanel.pause();	
+					JOptionPane.showMessageDialog(app.getMainFrame(), "game paused");				
+					
+				}
+				
 			}
 		
-			//ecs key pressed to close the "game is paused" dialogue
-			if(key == 27) {
-				//TODO close the game pause panel
-				
-				countdownPanel.start();								
-			}
+		
 			
-			//CTRL-X is pressed to exit game no save
+			/*----------------CTRL-X is pressed to exit game no save------------*/
 			if ((e.getKeyCode() == KeyEvent.VK_X) && ((e.getModifiersEx() & KeyEvent.CTRL_DOWN_MASK) != 0)) {
-				frame.dispose();
+				app.terminateFrame();
 			}
 		
-			//CTRL-S is pressed to save and exit the game
+			/*----------------CTRL-S is pressed to save and exit the game-------*/
 			if ((e.getKeyCode() == KeyEvent.VK_S) && ((e.getModifiersEx() & KeyEvent.CTRL_DOWN_MASK) != 0)) {
-				//TODO save the game
-				XMLFileWriter fileWriter = new XMLFileWriter();	
 				
-				frame.dispose();
+				//TODO save the game
+				XMLFileWriter fileWriter = new XMLFileWriter();					
+				app.terminateFrame();
 			}
 		
 			//CTRL-R resume a saved game
@@ -119,7 +134,15 @@ public class KeyPresses implements KeyListener {
 			}
 		
 		}
+		
+		// if game is at paused
 		else {
+			// resume game and close the game pause dialog
+			if(key == 27) {
+				//TODO close the game pause panel
+					   countdownPanel.start();				   						
+			}
+
 			System.out.println("hahahahahahahah");
 		}
 		
