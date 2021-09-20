b9d1311fb0972a732d3ea52a83653c54810c22ab
Nguyen Van <vannguy1@ecs.vuw.ac.nz>
2021-09-20 22:01:16 +1200

    Reupdate the countdown panel, as I updated the wrong file

--- src/nz/ac/vuw/ecs/swen225/gp21/app/CountdownPanel.java
+++ src/nz/ac/vuw/ecs/swen225/gp21/app/CountdownPanel.java
@@ -1,271 +1,321 @@
 package nz.ac.vuw.ecs.swen225.gp21.app;
 
-import java.awt.*;
-import java.awt.event.*;
-import java.awt.image.BufferedImage;
-import java.util.ArrayList;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.*;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.Timer;
 import javax.swing.border.Border;
 
 import nz.ac.vuw.ecs.swen225.gp21.domain.Domain;
-import nz.ac.vuw.ecs.swen225.gp21.domain.actor.Enemy;
-import nz.ac.vuw.ecs.swen225.gp21.domain.actor.Player;
-import nz.ac.vuw.ecs.swen225.gp21.domain.board.Board;
-import nz.ac.vuw.ecs.swen225.gp21.domain.board.Item;
-import nz.ac.vuw.ecs.swen225.gp21.domain.utils.Direction;
-import nz.ac.vuw.ecs.swen225.gp21.persistency.writer.XMLFileWriter;
-import nz.ac.vuw.ecs.swen225.gp21.recorder.Recorder;
-import nz.ac.vuw.ecs.swen225.gp21.renderer.util.ImagePaths;
 
 /**
- * Class for the key presses implementation.
+ * The class countdown panel that creates the countdown panel
+ * on the right of the main frame.
  */
-public class KeyPresses implements KeyListener {
-
-	private Player hero;
-	private Domain domain;
-	private CountdownPanel countdownPanel;
+public class CountdownPanel implements ActionListener {
+	
+//	private Board board;
+	private int seconds_remaining;
+	private int chips_left_value;
+	private int level_value;
 	private App app;
-	private JPanel pausedDialoge;
-	private int inventoryCount;
 	
-	//TODO construct a recorder and do recording stuff
+    //private boolean replay = false;
+
+	private boolean started = false;
+		
+	private JLayeredPane countdownPanel = new JLayeredPane(); 
+	private JLabel time_left = new JLabel();
+	private JLabel level = new JLabel();
+	private JLabel chips_left = new JLabel();
+    private JButton start_pause = new JButton("START");
+    
+    private JPanel inventory;
+    private JLabel[] item_slots = new JLabel[8];
+   
 	
-	public KeyPresses(App app, Domain domain) {
+	Timer timer = new Timer(1000, new ActionListener() {
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+			seconds_remaining = seconds_remaining-1;
+			String seconds_string = String.format(String.valueOf(seconds_remaining));
+			time_left.setText(seconds_string);		
+						
+			// if timer reaches 0, the player looses
+			if(seconds_remaining==0) {
+				
+				timer.stop();
+				int confirm = JOptionPane.showOptionDialog(
+                        app.getMainFrame(), "You have ran out of time, restart this level?",
+                        "Exit Confirmation", JOptionPane.YES_NO_OPTION,
+                        JOptionPane.QUESTION_MESSAGE, null, null, null);
+				
+				//if player wants to replay this level
+				if(confirm == 0) {
+					
+					String restartLevel= "level"+level_value;
+					app.terminateFrame();
+					new App(restartLevel);
+					
+				}
+								
+				//else go to the title screen
+				else {
+					
+					app.terminateFrame();
+					new TitleScreen();
+					
+				}
+			}
+			
+		}});
+	
+	
+	 /**
+     * Constructor for the coundown panel.
+     *
+     * @param time duration given in the level
+     * @param current_level level player is playing
+     * @param remaining_chips number of chips on board in the level
+     */
+	
+	public CountdownPanel(int time, int current_level, int chipsleft, App app) {
 		
 		this.app = app;
-		this.domain = domain;
-		this.hero = domain.getPlayer();	
-		this.countdownPanel = app.getCoundownPanel();
-		this.inventoryCount = hero.getInventory().size();
+		this.seconds_remaining = time;
+		this.chips_left_value = chipsleft;
+		this.level_value = current_level;
+		this.setUpInventoryPanel();
 		
-		app.getMainFrame().addKeyListener(this);
 		
-		pausedDialoge = this.setPausedDialoge();
-			
-		app.getMainFrame().add(pausedDialoge);
+		//this.board = board;
+		start_pause.addActionListener(this);
+		start_pause.setFocusable(false);
+		seconds_remaining = time;	
 		
-	
-	}
+		/*----------------Initialising all the JLabels----------------------------------*/
+		JLabel timer_title = titleLabel("TIME", Color.RED);		
+        JLabel chips_title = titleLabel("CHIPS",Color.RED);  
+        JLabel left_title  = titleLabel("LEFT", Color.RED);
+        JLabel level_title = titleLabel("LEVEL",Color.RED);
+        
+		JLabel timer_title2 = titleLabel("TIME", Color.WHITE);		
+        JLabel chips_title2 = titleLabel("CHIPS",Color.WHITE);  
+        JLabel left_title2  = titleLabel("LEFT", Color.WHITE);
+        JLabel level_title2 = titleLabel("LEVEL",Color.WHITE);
+		/*------------------------------------------------------------------------------*/
+        
+               
+		/*----------------Initialising all the boxes to put numbers in------------------*/
+        time_left = buttonTitleSetup(String.valueOf(time));	        
+        chips_left = buttonTitleSetup(String.valueOf(chipsleft));        		        
+        level = buttonTitleSetup(String.valueOf(current_level)); 
+		/*------------------------------------------------------------------------------*/
+        
 
+		/*----------------Setting up the locations for each label-----------------------*/
+        timer_title.setBounds(75, 30, 96, 35);
+        timer_title2.setBounds(76, 28, 96, 35);
+        time_left.setBounds(55, 65, 96, 40);
+        chips_title.setBounds(75, 115, 96, 35);
+        chips_title2.setBounds(76, 113, 96, 35);
+        left_title.setBounds(80,140,96,35);
+        left_title2.setBounds(81,138,96,35);
+        chips_left.setBounds(55, 175, 96, 40);
+        level_title.setBounds(72, 225, 96, 35);
+        level_title2.setBounds(73, 223, 96, 35);
+        level.setBounds(55, 260, 96, 40);
+        start_pause.setBounds(60, 350, 90, 40);
+		/*------------------------------------------------------------------------------*/
+        
+        countdownPanel.setLayout(null);
+        countdownPanel.add(timer_title);
+        countdownPanel.add(time_left);
+        countdownPanel.add(level_title);
+        countdownPanel.add(level);
+        countdownPanel.add(chips_title);
+        countdownPanel.add(left_title);
+        countdownPanel.add(chips_left);    
+        countdownPanel.add(start_pause);
+        countdownPanel.add(timer_title2);
+        countdownPanel.add(chips_title2);
+        countdownPanel.add(left_title2);
+        countdownPanel.add(level_title2);
+                
+       
+        countdownPanel.setBackground(Color.LIGHT_GRAY);
+        countdownPanel.setBorder(BorderFactory.createRaisedBevelBorder());
+        
+        
+                           
+	}	
+	
+	 /**
+     * This method returns to countdown panel.
+     *
+     * @return the countdown panel
+     */
+	public JLayeredPane getPanel() {
+		return countdownPanel;
+	}
+	
+
+	
+	
 	/**
-	 * actions when a key is pressed.
+     * This method sets up JLabel for all the time, chips left, level labels on the panel 
+     * with roboto fond, bold, size 20 and coloured in red
+     *
+     * @param title name of the title
+     * @return a new JLabel
+     */
+	public JLabel buttonTitleSetup(String title) {	
+		
+		JLabel label = new JLabel(title);   
+		label.setFont(new Font("Roboto",Font.BOLD,20));
+		label.setForeground(Color.BLACK);
+		label.setOpaque(true);
+		label.setBorder(BorderFactory.createBevelBorder(1));
+		label.setHorizontalAlignment(JTextField.RIGHT);
+		
+		return label;		
+		
+	}
+	
+	/**
+     * This method sets up JLabel for all the time, chips left, level labels on the panel 
+     * with roboto fond, bold, size 20 and coloured in red
+     *
+     * @param title name of the title
+     * @param col of the font
+     * @return a new JLabel
+     */
+	public JLabel titleLabel(String title, Color col) {
+		
+		JLabel label = new JLabel(title);   
+		label.setForeground(col);
+		label.setFont(new Font("Roboto",Font.BOLD,20));		
+		
+		return label;	
+		
+	}
+	
+	 /**
+     * This method is to update the remaining chips on the right hand panel
+     *
+     * @param remaining_chips the remaining number of chips on board
+     */
+
+	public void updateRemainingChip(int remaining_chips) {		
+		chips_left.setText(String.valueOf(remaining_chips)); 		
+	}
+	
+	
+	/**
+	 * Execute running field in domain when start button is pressed.
+	 * Start button will disapear.
 	 * 
 	 * @param e event
 	 */
 	@Override
-	public void keyPressed(KeyEvent e) {
-				
-		int key = e.getKeyCode();
+	public void actionPerformed(ActionEvent e) {
 
-		/*----------------CTRL-1 start new game lv1-----------------------*/
-		if ((e.getKeyCode() == 49) && ((e.getModifiersEx() & KeyEvent.CTRL_DOWN_MASK) != 0)) {
-			
-			countdownPanel.pause();
-			
-            int confirm = JOptionPane.showOptionDialog(
-                    null, "Restart level 1?",
-                    "", JOptionPane.YES_NO_OPTION,
-                    JOptionPane.QUESTION_MESSAGE, null, null, null);
-            
-            // yes = 0, no = 1
-            if(confirm == 0) {           	
-            	app.terminateFrame();
-            	new App("level1");           	
-            }
-            			
-		}
-	
-		/*----------------CTRL-2 start new game lv2-----------------------*/
-		if ((e.getKeyCode() == 50) && ((e.getModifiersEx() & KeyEvent.CTRL_DOWN_MASK) != 0)) {
-			countdownPanel.pause();
-			
-            int confirm = JOptionPane.showOptionDialog(
-                    app.getMainFrame(), "Restart level 2?",
-                    "", JOptionPane.YES_NO_OPTION,
-                    JOptionPane.QUESTION_MESSAGE, null, null, null);
-            
-            // yes = 0, no = 1
-            if(confirm == 0) {           	
-            	app.terminateFrame();
-            	new App("level2");           	
-            }
-			
-		}
-		/*----------------------------------------------------------------*/
-		
-		
-			
-		/*-----------WHEN GAME IS STARTED---------------------------------*/
-		
-		if(countdownPanel.getStarted()) {
-			
-			String infoMessage = hero.listenForMessage();
-			
-			if(key == 37) {
-				domain.moveActor(this.hero, Direction.WEST);
-				app.updateChipsCount();
-				app.getRecorder().saveAction(Direction.WEST, hero, countdownPanel.getTimer());
-				//TODO finish doing the update inventory
-				updateInventoryPanel();
-			}
-			
-			if(key == 38) {
-				domain.moveActor(this.hero, Direction.NORTH);
-				app.updateChipsCount();	
-				app.getRecorder().saveAction(Direction.NORTH, hero, countdownPanel.getTimer());
-				updateInventoryPanel();
-								
-				}
-					
-			if(key == 39) {
-				domain.moveActor(this.hero, Direction.EAST);
-				app.updateChipsCount();
-				app.getRecorder().saveAction(Direction.EAST, hero, countdownPanel.getTimer());
-				updateInventoryPanel();
-			}
-	
-			if(key == 40) {
-				domain.moveActor(this.hero, Direction.SOUTH);
-				app.updateChipsCount();
-				app.getRecorder().saveAction(Direction.SOUTH, hero, countdownPanel.getTimer());
-				updateInventoryPanel();
-			}
-				
-			//SPACE
-			if(key == 32) {
-					
-				countdownPanel.pause();					
-				countdownPanel.getPanel().setVisible(false);
-				app.getRenderView().setVisible(false);
-				pausedDialoge.setVisible(true);	
-				//TODO set the game frame invisible too
-					
-			}
-			
-			
-				
-			/*----------------CTRL-X is pressed to exit game no save------------*/
-			if ((e.getKeyCode() == KeyEvent.VK_X) && ((e.getModifiersEx() & KeyEvent.CTRL_DOWN_MASK) != 0)) {
-				app.terminateFrame();
-			}
-				
-			
-			/*----------------CTRL-S is pressed to save and exit the game-------*/
-			if ((e.getKeyCode() == KeyEvent.VK_S) && ((e.getModifiersEx() & KeyEvent.CTRL_DOWN_MASK) != 0)) {
-				System.out.println("ctrl-s pressed");
-					
-				//TODO save the game (a lot to do here)
-					
-				XMLFileWriter fileWriter = new XMLFileWriter();	
-				app.terminateFrame();
-			}
-				
-			
-			/*----------------CTRL-R open saved games file and resume a game----*/
-			if ((e.getKeyCode() == KeyEvent.VK_R) && ((e.getModifiersEx() & KeyEvent.CTRL_DOWN_MASK) != 0)) {
-				
-			}
-				
-			if(!app.getDomain().isRunning()) {
-				countdownPanel.stop();
-				JOptionPane.showMessageDialog(app.getMainFrame(), "You finished the level!");					
-			}
-			
-			//TODO do the popup info message
-//			if(infoMessage != null) {
-//				System.out.println("this is null kefe");
-//				JOptionPane.showMessageDialog(app.getMainFrame(), infoMessage);	
-//			}
-										
-		}
-			
-		// ESC
-		else {
-				
-			if(key == 27) {
-				//TODO set the main frame true too
-				countdownPanel.getPanel().setVisible(true);
-				app.getRenderView().setVisible(true);
-				pausedDialoge.setVisible(false);
-				countdownPanel.start();	
-			}
-	
-			System.out.println("game is paused kefe");
-		}
-
-	}
-	
-
-	@Override
-	public void keyReleased(KeyEvent e) {
+		  if(e.getSource() == start_pause) {
+			  app.getRenderView().startRender();
+			  app.getDomain().setRunning(true);
+			  start();	
+			  start_pause.setVisible(false);
+			  inventory.setVisible(true);
+			   
+		  }
 		
 	}
 	
-	@Override
-	public void keyTyped(KeyEvent e) {
-	
+	/**
+     * This method is called when the game starts or to resume the game
+     * and also change the button label.
+     */
+	public void start() {
+		started = true;
+		timer.start();
 	}
 	
-	public void updateInventoryPanel() {
+	/**
+     * This method is called when the game is paused which timer will stop counting bu
+     * and also change the button label.
+     */	
+	public void pause() {
+		started = false;		
+		timer.stop();
+	}	
+	
+	/**
+     * This method is called when the game is lost which will stop the timer and reset the level.
+     */	
+	public void stop() {
+		started = false;
+		timer.stop();		
+	}
+	
+	public void setUpInventoryPanel() {
 		
-		if(hero.getInventory().size() != inventoryCount && hero.getInventory().size()!=0 ) {
-			
-			ArrayList<Item> item2 = hero.getInventory();
-			JLabel[] itemsPanel = countdownPanel.getItemPanel();
-			Item[] items = new Item[6];
-			
-			for(int i=0; i<item2.size(); i++) {
-				items[i] = item2.get(i);
-			}
-			
-			
-			for(int i=0; i<6; i++) {
-				
-				if(items[i] != null) {
-
-					String id = items[i].getId();
-					System.out.println(id);
-					itemsPanel[i].setIcon(new ImageIcon("res/graphics/key.png"));						
-				}
-				
-				else {
-					System.out.println("doing this");
-					JLabel picture = new JLabel();
-					picture.setBounds(0,0,64,64);					
-					itemsPanel[i].setIcon(null);				
-				}
-			}
-			
-			this.inventoryCount = hero.getInventory().size();
-			
+		inventory = new JPanel();
+		inventory.setBounds(25, 340, 192, 128);
+		inventory.setLayout(new GridLayout(2,3));
+		inventory.setVisible(false);
+		
+		for(int i=0; i<6; i++) {
+			item_slots[i] = new JLabel();
+			// TODO might check this line
+			item_slots[i].setBorder(BorderFactory.createRaisedBevelBorder());
+			item_slots[i].setBackground(Color.LIGHT_GRAY);
+			inventory.add(item_slots[i]);
 		}
 		
+		
+		countdownPanel.add(inventory);
+		
 	}
 	
-	public JPanel setPausedDialoge() {
-		
-		Border border = BorderFactory.createLineBorder(Color.BLACK, 8);
-		pausedDialoge = new JPanel();
-		pausedDialoge.setBounds(475,300,300,200);
-		pausedDialoge.setBackground(new Color(54,49,48));
-		pausedDialoge.setBorder(border);
-		pausedDialoge.setVisible(false);
-		pausedDialoge.setLayout(null);
-		
-		JLabel pausedText = new JLabel("Paused");
-		pausedText.setFont(new Font("Monospaced", Font.BOLD, 35));
-		pausedText.setForeground(Color.WHITE);
-		pausedText.setBounds(82,80,200,40);
-		pausedDialoge.add(pausedText);
-		
-		JLabel pausedText2 = new JLabel("Paused");
-		pausedText2.setFont(new Font("Monospaced", Font.BOLD, 35));
-		pausedText2.setForeground(Color.BLACK);
-		pausedText2.setBounds(82,82,200,40);
-		pausedDialoge.add(pausedText2);
-		
-		return pausedDialoge;
+	public JLabel[] getItemPanel() {
+		return item_slots;
 	}
+	
+	public JPanel getInventoryPanel() {
+		return inventory;
+	}
+	
+	
+	/**
+	 * Return if the game is started or paused.
+	 * 
+	 * @return started or not
+	 */
+	public boolean getStarted() {
+		return started;
+	}
+	
+	//--------------GETTER METHODS----------------------------------
+	
+	public int getTimer() {
+		return seconds_remaining;				
+	}
+	
+	public int getRemainingTreasures() {
+		return chips_left_value;
+	}	
 }
