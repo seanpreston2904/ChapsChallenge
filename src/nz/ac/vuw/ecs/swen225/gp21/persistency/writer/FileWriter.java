966ddaf5142bc1abc8dabf95af78c4eaa941c426
rae <jkraezou@gmail.com>
2021-09-20 12:18:10 +1200

    - added FileReader interface and FileWriter interface. - moved XMLFileReader and XMLFileWriter from persistency to persistency\reader & persistency\writer.

---
+++ src/nz/ac/vuw/ecs/swen225/gp21/persistency/writer/FileWriter.java
@@ -0,0 +1,33 @@
+package nz.ac.vuw.ecs.swen225.gp21.persistency.writer;
+
+import nz.ac.vuw.ecs.swen225.gp21.domain.board.Board;
+import org.dom4j.Element;
+
+/**
+ * An FileWriter interface to achieve the following abstraction.
+ * 1. save the current game map.
+ * 2. save the current game records in order for the player to resume games.
+ *
+ * @author Rae 300535154
+ */
+
+public interface FileWriter {
+
+    /**
+     * save the board of the current game.
+     *  @param fName the output file name
+     * @param board the current board
+     */
+    void saveCurrentMap(String fName, Board board);
+
+    /**
+     *  add the game actions to XML.
+     *  This is for Recorder to write each action records to a XML file.
+     *
+     * @param root the root ele
+     * @param timer current time left
+     * @param actor current actor
+     * @param dir direction actor moved
+     */
+    void addActionNode(Element root, int timer, String actor, String dir);
+}
