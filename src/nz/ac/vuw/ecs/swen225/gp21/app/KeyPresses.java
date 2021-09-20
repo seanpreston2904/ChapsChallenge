ea63741f76b6f98607e678895ef510b1404b3896
Nguyen Van <vannguy1@ecs.vuw.ac.nz>
2021-09-20 22:04:37 +1200

    Update display for inventory

--- src/nz/ac/vuw/ecs/swen225/gp21/app/KeyPresses.java
+++ src/nz/ac/vuw/ecs/swen225/gp21/app/KeyPresses.java
@@ -15,6 +15,7 @@ import nz.ac.vuw.ecs.swen225.gp21.domain.board.Board;
 import nz.ac.vuw.ecs.swen225.gp21.domain.board.Item;
 import nz.ac.vuw.ecs.swen225.gp21.domain.utils.Direction;
 import nz.ac.vuw.ecs.swen225.gp21.persistency.writer.XMLFileWriter;
+import nz.ac.vuw.ecs.swen225.gp21.recorder.Recorder;
 import nz.ac.vuw.ecs.swen225.gp21.renderer.util.ImagePaths;
 
 /**
@@ -59,119 +60,112 @@ public class KeyPresses implements KeyListener {
 		int key = e.getKeyCode();
 
 		/*----------------CTRL-1 start new game lv1-----------------------*/
-		if ((e.getKeyCode() == 49) && ((e.getModifiersEx() & KeyEvent.CTRL_DOWN_MASK) != 0)) {
-			
-			countdownPanel.pause();
-			
-            int confirm = JOptionPane.showOptionDialog(
-                    null, "Restart level 1?",
-                    "", JOptionPane.YES_NO_OPTION,
-                    JOptionPane.QUESTION_MESSAGE, null, null, null);
-            
-            // yes = 0, no = 1
-            if(confirm == 0) {           	
-            	app.terminateFrame();
-            	new App("level1");           	
-            }
-            			
+		if ((e.getKeyCode() == 49) && ((e.getModifiersEx() & KeyEvent.CTRL_DOWN_MASK) != 0)) {			
+			startNewGame(1);            			
 		}
 	
 		/*----------------CTRL-2 start new game lv2-----------------------*/
+		
 		if ((e.getKeyCode() == 50) && ((e.getModifiersEx() & KeyEvent.CTRL_DOWN_MASK) != 0)) {
-			countdownPanel.pause();
+			startNewGame(2);			
+		}		
+		/*----------------------------------------------------------------*/
+		
+		
 			
-            int confirm = JOptionPane.showOptionDialog(
-                    app.getMainFrame(), "Restart level 2?",
-                    "", JOptionPane.YES_NO_OPTION,
-                    JOptionPane.QUESTION_MESSAGE, null, null, null);
-            
-            // yes = 0, no = 1
-            if(confirm == 0) {           	
-            	app.terminateFrame();
-            	new App("level2");           	
-            }
-			
-		}
+		/*-----------WHEN GAME IS STARTED---------------------------------*/
 		
-		
-		//TODO every time a move is made update the inventory
-		
-		//only perform action when the game is not paused
 		if(countdownPanel.getStarted()) {
+			
+			String infoMessage = hero.listenForMessage();
+			
 			if(key == 37) {
-				domain.moveActor(this.hero, Direction.WEST);
-				app.updateChipsCount();
-				updateInventoryPanel();
+				moveHero(Direction.WEST);
 			}
-		
+			
 			if(key == 38) {
-				domain.moveActor(this.hero, Direction.NORTH);
-				app.updateChipsCount();
-				updateInventoryPanel();
+				moveHero(Direction.NORTH);								
+				}
+					
+			if(key == 39) {
+				moveHero(Direction.EAST);
+			}
+	
+			if(key == 40) {
+				moveHero(Direction.SOUTH);
 			}
 				
-			if(key == 39) {
-				domain.moveActor(this.hero, Direction.EAST);
-				app.updateChipsCount();
-				updateInventoryPanel();
-			}
-
-			if(key == 40) {
-				domain.moveActor(this.hero, Direction.SOUTH);
-				app.updateChipsCount();
-				updateInventoryPanel();
-			}
-			
 			//SPACE
 			if(key == 32) {
-				
-				countdownPanel.pause();	
+					
+				countdownPanel.pause();					
 				countdownPanel.getPanel().setVisible(false);
+				app.getRenderView().setVisible(false);
 				pausedDialoge.setVisible(true);	
 				//TODO set the game frame invisible too
-				
+					
 			}
-		
-		
 			
+			
+				
 			/*----------------CTRL-X is pressed to exit game no save------------*/
 			if ((e.getKeyCode() == KeyEvent.VK_X) && ((e.getModifiersEx() & KeyEvent.CTRL_DOWN_MASK) != 0)) {
-				app.terminateFrame();
+
+	            int confirm = JOptionPane.showOptionDialog(
+	                    app.getMainFrame(), "Save game before exiting?",
+	                    "", JOptionPane.YES_NO_OPTION,
+	                    JOptionPane.QUESTION_MESSAGE, null, null, null);
+	            
+	            if(confirm == 0) {           	
+	            	app.terminateFrame();          	
+	            }
+	            else {
+	            	saveGame();
+	            }
 			}
+				
 			
-		
 			/*----------------CTRL-S is pressed to save and exit the game-------*/
 			if ((e.getKeyCode() == KeyEvent.VK_S) && ((e.getModifiersEx() & KeyEvent.CTRL_DOWN_MASK) != 0)) {
 				System.out.println("ctrl-s pressed");
-				
-				//TODO save the game (a lot to do here)
-				
-				XMLFileWriter fileWriter = new XMLFileWriter();	
+					
+				//TODO save the game (a lot to do here)					
+				saveGame();
 				app.terminateFrame();
 			}
+				
 			
-		
 			/*----------------CTRL-R open saved games file and resume a game----*/
 			if ((e.getKeyCode() == KeyEvent.VK_R) && ((e.getModifiersEx() & KeyEvent.CTRL_DOWN_MASK) != 0)) {
-			
+				openSavedGame();
 			}
-		
+				
+			if(!app.getDomain().isRunning()) {
+				countdownPanel.stop();
+				JOptionPane.showMessageDialog(app.getMainFrame(), "You finished the level!");					
+			}
+			
+			//TODO do the popup info message
+//			if(infoMessage != null) {
+//				JOptionPane.showMessageDialog(app.getMainFrame(), infoMessage);	
+//			}
+										
 		}
-		
+			
 		// ESC
 		else {
-			
+				
 			if(key == 27) {
-				//TODO set the main frame true too
-					   countdownPanel.getPanel().setVisible(true);
-					   pausedDialoge.setVisible(false);
-					   countdownPanel.start();	
-			}
 
+				countdownPanel.getPanel().setVisible(true);
+				app.getRenderView().setVisible(true);
+				pausedDialoge.setVisible(false);
+				countdownPanel.start();	
+			}
+	
 			System.out.println("game is paused kefe");
 		}
-		
-		
+
 	}
 	
 
@@ -187,44 +181,95 @@ public class KeyPresses implements KeyListener {
 	
 	public void updateInventoryPanel() {
 		
-		if(hero.getInventory().size() != inventoryCount) {
-			//TODO update the panel
+		if(hero.getInventory().size() != inventoryCount && hero.getInventory().size()!=0 ) {
 			
-			ArrayList<Item> items = hero.getInventory();
-			Item item = items.get(0);
-			item.getId();
-			//BufferedImage IMG_TREASURE = ImagePaths.IMG_KEY;
-			JLabel picture = new JLabel();
-			picture.setBounds(10,10,20,20);
-			picture.setIcon(new ImageIcon("res/graphics/key.png"));
-			JPanel[] itemsPanel = countdownPanel.getItemPanel();
-			itemsPanel[0].add(picture);					
+			ArrayList<Item> item2 = hero.getInventory();
+			JLabel[] itemsPanel = countdownPanel.getItemPanel();
+			Item[] items = new Item[6];
+			
+			for(int i=0; i<item2.size(); i++) {
+				items[i] = item2.get(i);
+			}
+			
+			
+			for(int i=0; i<6; i++) {
+				
+				if(items[i] != null) {
+
+					String id = items[i].getId();
+					System.out.println(id);
+					itemsPanel[i].setIcon(new ImageIcon("res/graphics/key.png"));						
+				}
+				
+				else {
+					JLabel picture = new JLabel();
+					picture.setBounds(0,0,64,64);					
+					itemsPanel[i].setIcon(null);				
+				}
+			}
+			
+			this.inventoryCount = hero.getInventory().size();
+			
 		}
 		
 	}
 	
 	public JPanel setPausedDialoge() {
 		
-		Border border = BorderFactory.createLineBorder(Color.BLACK, 6);
+		Border border = BorderFactory.createLineBorder(Color.BLACK, 8);
 		pausedDialoge = new JPanel();
-		pausedDialoge.setBounds(340,200,200,140);
+		pausedDialoge.setBounds(475,300,300,200);
 		pausedDialoge.setBackground(new Color(54,49,48));
 		pausedDialoge.setBorder(border);
 		pausedDialoge.setVisible(false);
 		pausedDialoge.setLayout(null);
 		
 		JLabel pausedText = new JLabel("Paused");
-		pausedText.setFont(new Font("Monospaced", Font.BOLD, 25));
+		pausedText.setFont(new Font("Monospaced", Font.BOLD, 35));
 		pausedText.setForeground(Color.WHITE);
-		pausedText.setBounds(55,45,100,40);
+		pausedText.setBounds(82,80,200,40);
 		pausedDialoge.add(pausedText);
 		
 		JLabel pausedText2 = new JLabel("Paused");
-		pausedText2.setFont(new Font("Monospaced", Font.BOLD, 25));
+		pausedText2.setFont(new Font("Monospaced", Font.BOLD, 35));
 		pausedText2.setForeground(Color.BLACK);
-		pausedText2.setBounds(57,47,100,40);
+		pausedText2.setBounds(82,82,200,40);
 		pausedDialoge.add(pausedText2);
 		
 		return pausedDialoge;
+	}
+	
+	
+	
+	public void moveHero(Direction dir) {		
+		domain.moveActor(this.hero, dir);
+		app.updateChipsCount();
+		app.getRecorder().saveAction(dir, hero, countdownPanel.getTimer());
+		updateInventoryPanel();		
+	}
+	
+	public void saveGame() {
+		
+	}
+	
+	public void openSavedGame() {
+		
+	}
+	
+	public void startNewGame(int level) {
+		
+		countdownPanel.pause();
+		
+        int confirm = JOptionPane.showOptionDialog(
+                null, "Restart level "+level,
+                "", JOptionPane.YES_NO_OPTION,
+                JOptionPane.QUESTION_MESSAGE, null, null, null);
+        
+        // yes = 0, no = 1
+        if(confirm == 0) {           	
+        	app.terminateFrame();
+        	new App("level"+level);           	
+        }
+        
 	}
 }
