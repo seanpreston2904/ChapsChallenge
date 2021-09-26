659c9fd611ec6c90d6c30c7e35b7322f03eae158
NRais <raisnath@ecs.vuw.ac.nz>
2021-09-26 16:23:15 +1300

    Player death by Enemy
    Added death mechanics for player.
    Added check so that Enemies can't stand on each other.

--- src/nz/ac/vuw/ecs/swen225/gp21/domain/board/Board.java
+++ src/nz/ac/vuw/ecs/swen225/gp21/domain/board/Board.java
@@ -2,6 +2,7 @@ package nz.ac.vuw.ecs.swen225.gp21.domain.board;
 
 
 import nz.ac.vuw.ecs.swen225.gp21.domain.actor.Actor;
+import nz.ac.vuw.ecs.swen225.gp21.domain.actor.Player;
 import nz.ac.vuw.ecs.swen225.gp21.domain.utils.Coordinate;
 import nz.ac.vuw.ecs.swen225.gp21.domain.utils.ItemType;
 import nz.ac.vuw.ecs.swen225.gp21.domain.utils.TileType;
